13e13af923692edce5132010bce17f875a03c3be
Saji Pova <sajipova@Sajis-MacBook-Air.local>
2026-05-29 12:53:02 -0500

    Temporarily disable Sanity

--- src/lib/cms/getHomepageHeader.ts
+++ src/lib/cms/getHomepageHeader.ts
@@ -1,15 +1,6 @@
 import { homepageHeaderPlaceholder } from "@/lib/cms/placeholders";
-import { getHomepageHeaderFromSanity } from "@/lib/sanity/queries";
 import type { HomepageHeaderContent } from "@/types/cms";
 
 export async function getHomepageHeader(): Promise<HomepageHeaderContent> {
-  if (
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID &&
-    process.env.NEXT_PUBLIC_SANITY_DATASET
-  ) {
-    const fromSanity = await getHomepageHeaderFromSanity();
-    if (fromSanity) return fromSanity;
-  }
-
   return homepageHeaderPlaceholder;
 }
